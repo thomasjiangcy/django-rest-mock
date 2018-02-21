6b79ab04548f4cad2afd5df5c258a4b09c5a0407
Thomas Jiang <thomas.jiang@dathena.io>
2018-02-21 18:24:26 +0800

    WIP: Refactoring

---
+++ rest_mock_server/express.py
@@ -0,0 +1,60 @@
+import os
+import shutil
+import subprocess
+import sys
+
+class ExpressServer:
+
+    def __init__(self):
+        self.template = """
+        const express = require("express");\n
+        const app = express();\n
+        {variables}\n
+        {functions}\n
+        {endpoints}\n
+        app.listen({port}, () => console.log("Django Rest Mock Server: Listening on port {port}"));\n
+        """
+        self.constructed = ''
+    
+    def __str__(self):
+        if self.constructed:
+            return self.constructed.strip().replace("\n", "")
+        raise Exception('Call construct() first')
+    
+    def to_string(self):
+        return str(self)
+    
+    def construct(self, variables, functions, endpoints, port=8000):
+        self.constructed = self.template.format(
+            variables=variables,
+            functions=functions,
+            endpoints=endpoints,
+            port=port
+        )
+    
+    def generate(self, file_path='index.js'):
+        with open(file_path, 'w') as f:
+            f.write(self.to_string())
+    
+    def start_server(self, file_path='index.js'):
+        # Check if node is installed
+        try:
+            subprocess.check_output(['node', '-v'])
+        except:
+            sys.stdout.write('Node.js not installed properly.\n')
+            sys.exit(0)
+
+        # Check if Express is installed
+        try:
+            subprocess.check_output(['npm', 'list', 'express'])
+        except:
+            subprocess.call(['npm', 'install', 'express'])
+
+        try:
+            subprocess.call(["node", file_path])
+        except KeyboardInterrupt:
+            os.remove(file_path)
+            path_prefix = os.getcwd()
+            os.remove(path_prefix + '/package-lock.json')
+            shutil.rmtree(path_prefix + '/node_modules')
+            sys.stdout.write('Mock server shutdown.\n')
--- rest_mock_server/structures.py
+++ rest_mock_server/structures.py
@@ -1,23 +1,23 @@
-class BaseFunctionFactory:
+class BaseFactory:
 
     def __init__(self):
         self.constructed = ''
         self.options = dict(strip=True, remove_space=True, strip_newline=False, strip_tab=False)
         self.is_constructed = False
-    
+
     def __str__(self):
         if self.is_constructed:
             func = self.constructed
 
             if self.options['strip']:
                 func = func.strip()
-            
+
             if self.options['remove_space']:
                 func = func.replace(" ", "")
-            
+
             if self.options['strip_newline']:
                 func = func.replace("\n", "")
-            
+
             if self.options['strip_tab']:
                 func = func.replace("\t", "")
 
@@ -26,7 +26,22 @@ class BaseFunctionFactory:
             raise Exception('Need to call construct() first')
 
 
-class Function(BaseFunctionFactory):
+class Variable(BaseFactory):
+
+    def __init__(self):
+        super().__init__()
+        self.constructed = "{var_type} {var_name} = {var_val};"
+    
+    def construct(self, var_type, var_name, var_val):
+        self.constructed = self.constructed.format(
+            var_type=var_type,
+            var_name=var_name,
+            var_val=var_val
+        )
+        self.is_constructed = True
+
+
+class Function(BaseFactory):
 
     def __init__(self):
         super().__init__()
@@ -44,17 +59,17 @@ class Function(BaseFunctionFactory):
         self.is_constructed = True
 
 
-class Endpoint(BaseFunctionFactory):
+class Endpoint(BaseFactory):
 
     def __init__(self):
         super().__init__()
         self.constructed = 'app.%s("%s", (req, res) => {%s});\n'
         self.options['strip_newline'] = False
-    
+
     def construct(self, method, uri, response):
         self.constructed = self.constructed % (method, uri, response)
         self.is_constructed = True
-    
+
     def construct_response(self, method, uri, response):
         """Shortcut to construct direct response"""
         self.construct(method, uri, 'res.send(%s)' % response)
