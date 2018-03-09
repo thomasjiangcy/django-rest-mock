083dd37df606d1615f8ece90e8e86993b87400fe
Thomas Jiang <thomas.jiang@dathena.io>
2018-03-09 16:31:18 +0800

    Updated version

--- rest_mock_server/__version__.py
+++ rest_mock_server/__version__.py
@@ -1,6 +1,6 @@
 __title__ = 'django-rest-mock'
 __description__ = 'Mock Express server generated based on your views, that can come in handy when developing REST APIs with Django'
-__version__ = '0.2.8'
+__version__ = '0.2.9'
 __author__ = 'Thomas Jiang'
 __author_email__ = 'thomasjiangcy@gmail.com'
 __url__ = 'https://github.com/thomasjiangcy/django-rest-mock-server'
